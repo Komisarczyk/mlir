ce8690c9a2d44980b63460019089de9ea9335364
Komisarczyk <konrad.komisarczyk@gmail.com>
2020-11-02 09:10:22 +0100

    commenting GPUAsyncRegionpass due to error

--- mlir/lib/Dialect/GPU/Transforms/AsyncRegionRewriter.cpp
+++ mlir/lib/Dialect/GPU/Transforms/AsyncRegionRewriter.cpp
@@ -108,10 +108,11 @@ struct GpuAsyncRegionPass::Callback {
 // inserts the necessary synchronization (as gpu.wait ops). Assumes sequential
 // execution semantics and that no GPU ops are asynchronous yet.
 void GpuAsyncRegionPass::runOnFunction() {
-  if (getFunction()
+ /* if (getFunction()
           .getRegion()
           .walk(Callback{OpBuilder(&getContext())})
           .wasInterrupted())
+    return signalPassFailure();*/
     return signalPassFailure();
 }
 
